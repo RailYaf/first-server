6e15e9f550b87255e7fa99b40fa68fd30d0632d4
RailYaf <qwertys3010944@mail.ru>
2021-01-06 19:34:17 +0400

    add server

---
+++ index.js
@@ -0,0 +1,70 @@
+var express = require("express"); //подключаем expess
+var bodyParser = require("body-parser");
+var fs = require("fs");
+
+const PORT = process.env.PORT || 5000 //создаем переменную PORT, чтобы обозначить на какому порту мы запускаем всё приложение
+
+const app = express() //создаем объект нашего приложения
+var jsonParser = bodyParser.json();
+
+app.use(express.static(__dirname + "/api"));
+
+app.get("/api/users", function(req, res){
+      
+    var content = fs.readFileSync("users.json", "utf8");
+    var users = JSON.parse(content);
+    res.send(users);
+});
+
+// получение одного пользователя по id
+app.get("/api/users/:id", function(req, res){
+      
+    var id = req.params.id; // получаем id
+    var content = fs.readFileSync("users.json", "utf8");
+    var users = JSON.parse(content);
+    var user = null;
+    // находим в массиве пользователя по id
+    for(var i=0; i<users.length; i++){
+        if(users[i].id==id){
+            user = users[i];
+            break;
+        }
+    }
+    // отправляем пользователя
+    if(user){
+        res.send(user);
+    }
+    else{
+        res.status(404).send();
+    }
+});
+
+// получение отправленных данных
+app.post("/api/users", jsonParser, function (req, res) {
+     
+    if(!req.body) return res.sendStatus(400);
+     
+    var userName = req.body.name;
+    var userPhone = req.body.phone;
+    var userEmail = req.body.email;
+    var user = {name: userName, phone: userPhone, email: userEmail};
+     
+    var data = fs.readFileSync("users.json", "utf8");
+    var users = JSON.parse(data);
+
+    // находим максимальный id
+    var id = Math.max.apply(Math,users.map(function(o){return o.id;}))
+    // увеличиваем его на единицу
+    user.id = id+1;
+    // добавляем пользователя в массив
+    users.push(user);
+    var data = JSON.stringify(users);
+    // перезаписываем файл с новыми данными
+    fs.writeFileSync("users.json", data);
+    res.send(user);
+});
+
+//обращаемся к объекту app, вызывая метод listen, котрая запускает сервер
+app.listen (5000, () => {
+    console.log('Server has been started')
+})
